9b0b05857510377d6a3c0114033b586423ce9dc5
animsaj <animsaj70@gmail.com>
2016-05-26 23:24:36 +0300

    adding files

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence').use(gulp);
+var util = require('gulp-util');
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') //Source all files ending with.scss in scss directory and its subdirectories
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({
+          stream: true
+      }))
+});
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('views/**/*.pug')
+  .pipe(pug({
+     pretty: true,
+  }))
+  .on('error', util.log)
+  .pipe(gulp.dest('app'))
+  .pipe(browserSync.reload({
+      stream: true
+  }))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('views/index.pug', ['views']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('useref', function() {
+    return gulp.src('app/*.html') //Source all html files
+      .pipe(useref())
+      .pipe(gulpIf('*.js', uglify())) //Minifies only if it is js file
+      .pipe(gulpIf('*.css', cssnano())) //Minifies only if it is css file
+      .pipe(gulp.dest('dist'))
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|gif|swg)')
+      .pipe(cache(imagemin({
+          gif: {
+              interlaced: true
+          }
+      })))
+      .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+      .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+    return  del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'views', 'browserSync', 'watch'],
+    callback
+  );
+});
+
+gulp.task('build', function(callback) {
+    runSequence('clean:dist', ['sass', 'useref', 'imagemin', 'fonts'], callback);
+});
